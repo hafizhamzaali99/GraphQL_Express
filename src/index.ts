0473896c3d50c3cb6b56d59036e3b4171f524980
hafizhamzaali <hamzaali.900900@gmail.com>
2024-08-01 15:25:22 +0500

    adding packages

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { connectGraphQL } from "./graphql/graphql";
-
+import cors from "cors";
+import morgan from "morgan";
 import { connectDB } from "./database/config";
 import dotenv from "dotenv";
 const app = express();
@@ -11,6 +12,8 @@ const port = Number(process.env.PORT) || 3000;
 connectDB(); //data base connection
 connectGraphQL(app); //graphql connection
 app.use(express.json());
+app.use(cors({ origin: " * ", credentials: true }));
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send("Hello Hamza");
